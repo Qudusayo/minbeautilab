ba90af0307dcac89c81c9a19821671057e2b6f2c
Qudusayo <zeekimono@gmail.com>
2020-04-28 00:00:57 +0100

    edited

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const exphbs = require("express-handlebars");
 const minifytext = require("./workers/minifiertext");
 const beautifytext = require("./workers/beautifiertext");
 const photoCopy = require("./workers/photoCopy");
+const request = require("request");
+const path = require("path");
 
 const app = express();
 
@@ -33,20 +35,112 @@ app.get("/minify", function (req, res) {
   res.render("minify");
 });
 
-app.post("/minify", (req, res) => {
-  const data = minifytext(req.body.lang, req.body.input);
-  res.render("minify", {
-    input: req.body.input,
-    output: data,
+app.post("/", (req, res) => {
+  request(req.body.url, (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      res.render("main", {
+        output: html,
+      });
+    }
   });
 });
 
+app.post("/minify", (req, res) => {
+  if (req.body.url) {
+    if (
+      req.body.url.split(".").reverse()[0].toUpperCase() == "CSS" ||
+      req.body.url.split(".").reverse()[0].toUpperCase() == "JSON" ||
+      req.body.url.split(".").reverse()[0].toUpperCase() == "HTML"
+    ) {
+      request(req.body.url, (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+          const data = minifytext(
+            req.body.url.split(".").reverse()[0].toUpperCase(),
+            html
+          );
+          res.render("minify", {
+            input: html,
+            output: data,
+          });
+        }
+      });
+    } else if (req.body.url.split(".").reverse()[0].toUpperCase() == "JS") {
+      request(req.body.url, (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+          const data = minifytext("Javascript", html);
+          res.render("minify", {
+            input: html,
+            output: data,
+          });
+        }
+      });
+    } else {
+      request(req.body.url, (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+          const data = minifytext("HTML", html);
+          res.render("minify", {
+            input: html,
+            output: data,
+          });
+        }
+      });
+    }
+  } else {
+    const data = minifytext(req.body.lang, req.body.input);
+    res.render("minify", {
+      input: req.body.input,
+      output: data,
+    });
+  }
+});
+
 app.post("/beautify", (req, res) => {
-  const data = beautifytext(req.body.lang, req.body.input);
-  res.render("beautify", {
-    input: req.body.input,
-    output: data,
-  });
+  if (req.body.url) {
+    if (
+      req.body.url.split(".").reverse()[0].toUpperCase() == "CSS" ||
+      req.body.url.split(".").reverse()[0].toUpperCase() == "JSON" ||
+      req.body.url.split(".").reverse()[0].toUpperCase() == "HTML"
+    ) {
+      request(req.body.url, (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+          const data = beautifytext(
+            req.body.url.split(".").reverse()[0].toUpperCase(),
+            html
+          );
+          res.render("beautify", {
+            input: html,
+            output: data,
+          });
+        }
+      });
+    } else if (req.body.url.split(".").reverse()[0].toUpperCase() == "JS") {
+      request(req.body.url, (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+          const data = beautifytext("Javascript", html);
+          res.render("beautify", {
+            input: html,
+            output: data,
+          });
+        }
+      });
+    } else {
+      request(req.body.url, (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+          const data = beautifytext("HTML", html);
+          res.render("beautify", {
+            input: html,
+            output: data,
+          });
+        }
+      });
+    }
+  } else {
+    const data = beautifytext(req.body.lang, req.body.input);
+    res.render("beautify", {
+      input: req.body.input,
+      output: data,
+    });
+  }
 });
 
 const port = 3000;
