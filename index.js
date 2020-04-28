1dcd978f904c02ca471ea062782f40c9bac0a6ed
Qudusayo <zeekimono@gmail.com>
2020-04-28 01:49:03 +0100

    edited

--- index.js
+++ index.js
@@ -143,5 +143,5 @@ app.post("/beautify", (req, res) => {
   }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
